13ebd93037a7394aa17d0b27383d833edac2695f
wallneradam <adam.wallner@gmail.com>
2018-09-10 00:08:05 +0200

    Renamed to flsynth, because SDL is not working on android. SF3 can be disabled with "-DDISABLE_SF3" cmake parameter

--- flsynth.c
+++ flsynth.c
@@ -1,9 +1,8 @@
-#include "sdl2-synth.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include "flsynth.h"
 #include "fluidlite.h"
 #include <SDL2/SDL.h>
 
@@ -13,16 +12,23 @@ void fini() __attribute__((destructor));
 
 
 static inline void synthesize(synth_t *synth) {
+//    // stereo
+//    if (synth->channels == 2) {
+//        fluid_synth_write_float(synth->fluid_synth, FRAME_PER_CYCLE, synth->buff, 0, 2, synth->buff, 1, 2);
+//    }
+//    // mono
+//    else {
+//        fluid_synth_write_float(synth->fluid_synth, FRAME_PER_CYCLE, synth->buff, 0, 1,
+//                                synth->buff + FRAME_PER_CYCLE * 4, 0, 1);
+//    }
     // stereo
     if (synth->channels == 2) {
-        fluid_synth_write_s16(synth->fluid_synth, FRAME_PER_CYCLE, synth->buff, 0, 2,
-                              synth->buff, 1, 2);
+        fluid_synth_write_s16(synth->fluid_synth, FRAME_PER_CYCLE, synth->buff, 0, 2, synth->buff, 1, 2);
     }
-    // mono
+        // mono
     else {
         fluid_synth_write_s16(synth->fluid_synth, FRAME_PER_CYCLE, synth->buff, 0, 1,
-                              synth->buff + FRAME_PER_CYCLE * 2, 0, 1);
-        // TODO: average the 2 channels together
+                              synth->buff + FRAME_PER_CYCLE * 4, 0, 1);
     }
 }
 
@@ -39,22 +45,31 @@ static int synthesize_thread(synth_t *synth) {
 }
 
 
+static int frames = 0;
+static float val = 0.7;
+
 static void audio_callback(synth_t *synth, unsigned char *stream, size_t stream_length) {
+//    printf("Length: %zu\n", stream_length);
     // Just copy precalculated buffer
     memcpy(stream, synth->buff, stream_length);
+//    float *float_stream = (float *) stream;
+//    for (int i=0; i < stream_length >> 2; i++) {
+//        if (frames++ % 100) val *= -1;
+//        float_stream[i] = val;
+//    }
     // Start next calculation
     SDL_SemPost(synth->proc_sem);
 }
 
 
-synth_t *sdl_synth_create(unsigned int sample_rate, unsigned char channels) {
+synth_t *flsynth_create(unsigned int sample_rate, unsigned char channels) {
     SDL_InitSubSystem(SDL_INIT_AUDIO);
 
     synth_t *synth = calloc(sizeof(synth_t), 1);
 
     synth->settings = new_fluid_settings();
     synth->fluid_synth = new_fluid_synth(synth->settings);
-    synth->buff = malloc((size_t) (FRAME_PER_CYCLE * 4)); // 5 -> 16 bit stereo
+    synth->buff = malloc((size_t) (FRAME_PER_CYCLE * 8)); // 8 -> 32 bit float * 2 channel (stereo)
     fluid_synth_set_sample_rate(synth->fluid_synth, sample_rate);
 
     synth->sample_rate = sample_rate;
@@ -63,7 +78,7 @@ synth_t *sdl_synth_create(unsigned int sample_rate, unsigned char channels) {
     return synth;
 }
 
-void sdl_synth_free(synth_t *synth) {
+void flsynth_free(synth_t *synth) {
     delete_fluid_synth(synth->fluid_synth);
     delete_fluid_settings(synth->settings);
 
@@ -73,18 +88,18 @@ void sdl_synth_free(synth_t *synth) {
     SDL_QuitSubSystem(SDL_INIT_AUDIO);
 }
 
-int sdl_synth_sfload(synth_t *synth, const char *filename, bool program_reset) {
+int flsynth_sfload(synth_t *synth, const char *filename, bool program_reset) {
     int res = fluid_synth_sfload(synth->fluid_synth, filename, program_reset);
     if (res > 0) synth->last_sfid = (unsigned int) res;
     return synth->last_sfid;
 }
 
 
-bool sdl_synth_start(synth_t *synth) {
+bool flsynth_start(synth_t *synth) {
     // Create audio specification
     SDL_AudioSpec audioSpec;
     audioSpec.freq = synth->sample_rate;
-    audioSpec.format = AUDIO_S16;
+    audioSpec.format = AUDIO_S16LSB;
     audioSpec.channels = synth->channels;
     audioSpec.samples = FRAME_PER_CYCLE;
     audioSpec.size = 0;
@@ -97,42 +112,45 @@ bool sdl_synth_start(synth_t *synth) {
     // Create a semaphore for thread synchronization
     synth->proc_sem = SDL_CreateSemaphore(0);
     // Start background calculation
-    synth->proc_thread = SDL_CreateThread((SDL_ThreadFunction) synthesize_thread, "sdl_synth_processing_thread", synth);
+    synth->proc_thread = SDL_CreateThread((SDL_ThreadFunction) synthesize_thread, "flsynth_processing_thread", synth);
 
     // Open audio device
-    synth->audio_device = SDL_OpenAudioDevice(NULL, 0, &audioSpec, &audioSpec, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
+    synth->audio_device = 1; SDL_OpenAudio(&audioSpec, NULL);
+//    synth->audio_device = SDL_OpenAudioDevice(NULL, 0, &audioSpec, NULL, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
     // Start playing
     SDL_PauseAudioDevice(synth->audio_device, 0);
 }
 
-void sdl_synth_stop(synth_t *synth) {
+void flsynth_stop(synth_t *synth) {
     SDL_CloseAudioDevice(synth->audio_device);
     synth->audio_device = 0;
     SDL_SemPost(synth->proc_sem);
 }
 
 
-bool sdl_synth_program_select_sfid(synth_t *synth, int chan, unsigned int sfid, unsigned int bank, unsigned int preset) {
+bool flsynth_program_select_sfid(synth_t *synth, int chan, unsigned int sfid, unsigned int bank, unsigned int preset) {
     return fluid_synth_program_select(synth->fluid_synth, chan, sfid, bank, preset) == 0;
 }
 
-bool sdl_synth_program_select(synth_t *synth, int chan, unsigned int bank, unsigned int preset) {
-    return sdl_synth_program_select_sfid(synth, chan, synth->last_sfid, bank, preset);
+bool flsynth_program_select(synth_t *synth, int chan, unsigned int bank, unsigned int preset) {
+    return flsynth_program_select_sfid(synth, chan, synth->last_sfid, bank, preset);
 }
 
 
-bool sdl_synth_noteon(synth_t *synth, int chan, int key, int velocity) {
+bool flsynth_noteon(synth_t *synth, int chan, int key, int velocity) {
     return fluid_synth_noteon(synth->fluid_synth, chan, key, velocity) == 0;
 }
 
-bool sdl_synth_noteoff(synth_t *synth, int chan, int key) {
+bool flsynth_noteoff(synth_t *synth, int chan, int key) {
     return fluid_synth_noteoff(synth->fluid_synth, chan, key) == 0;
 }
 
 
-bool sdl_synth_system_reset(synth_t *synth) {
+bool flsynth_system_reset(synth_t *synth) {
     return fluid_synth_system_reset(synth->fluid_synth) == 0;
 }
+
+
 
 
 static void _dummy_log() {}
---
+++ flsynth.h
@@ -0,0 +1,79 @@
+#ifndef FLSYNTH_LIBRARY_H
+#define FLSYNTH_LIBRARY_H
+
+#include <stdbool.h>
+
+// Howmany frames are processed in one cycle,
+//  if this is greater you will have more latency, but more stable sound
+#define FRAME_PER_CYCLE 512
+
+#ifdef __ANDROID__
+#include <android/log.h>
+// Redirect printf fnctions to android log
+#define printf(fmt, ...) __android_log_print(ANDROID_LOG_DEFAULT, "flsynth-printf", fmt, __VA_ARGS__)
+#endif
+
+
+typedef struct {
+    unsigned int sample_rate;
+    unsigned char channels;
+
+    void* settings;
+    void* fluid_synth;
+    unsigned int last_sfid;
+    void *buff;
+
+    unsigned int audio_device;
+
+    void *proc_thread;
+    void *proc_sem;
+} synth_t;
+
+
+/**
+ * Create synthesizer
+ * @return A struct contains the state of the synth object
+ */
+synth_t *flsynth_create(unsigned int sample_rate, unsigned char channels);
+
+/**
+ * Free the synth object
+ * @param synth A synth object to be free
+ */
+void flsynth_free(synth_t *synth);
+
+/**
+ * Load soundfont
+ * Soundfont can be sf2 and sf3 (ogg vorbis) format
+ * @param synth Synth object
+ * @param filename Path of soundfont file
+ * @param program_reset
+ * @return The soundfont ID
+ */
+int flsynth_sfload(synth_t *synth, const char *filename, bool program_reset);
+
+/**
+ * Start synthesizer
+ * Start the audio engine, the synthesis in the background, and send the result of the synth to the audio output.
+ * @param synth Synth object
+ * @return
+ */
+bool flsynth_start(synth_t *synth);
+
+/**
+ * Stop the synthesis thread and audio engine.
+ * @param synth Synth object
+ */
+void flsynth_stop(synth_t *synth);
+
+
+bool flsynth_program_select(synth_t *synth, int chan, unsigned int bank, unsigned int preset);
+bool flsynth_program_select_sfid(synth_t *synth, int chan, unsigned int sfid, unsigned int bank, unsigned int preset);
+
+bool flsynth_noteon(synth_t *synth, int chan, int key, int velocity);
+bool flsynth_noteoff(synth_t *synth, int chan, int key);
+
+bool flsynth_system_reset(synth_t *synth);
+
+
+#endif
